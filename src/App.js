39af2528aeb1ad55c18ecf779cb252219e5d6ec1
Park Yu Jin <luxoris02@sookmyung.ac.kr>
2023-04-18 12:07:19 +0900

    깃실습6장.App 최종

--- src/App.js
+++ src/App.js
@@ -1,47 +1,10 @@
 import React from 'react';
-import Todo from './Todo';
-import AddTodo from './AddTodo';
-import {Paper, List, Container}from "@material-ui/core";
+import Hello from './Hello';
 import './App.css';
-
-class App extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={
-      items :[
-        {id:0, title:"Todo 1", done:true},
-        {id:1, title:"Todo 2", done:false},
-      ],
-    };
-  }
-  add =(item)=>{
-    const thisItems =this.state.items;
-    item.id ="ID-"+ thisItems.length;
-    item.done =false;
-    thisItems.push(item);
-    this.setState({items:thisItems});
-    console.log("items:",this.state.items);
-  } 
-  render(){
-   var todoItems =this.state.items.length >0 &&(
-    <Paper style={{margin:16}}>
-      <List>
-      {this.state.items.map((item,idx)=>(
-      <Todo item={item} key={item.id}/>
-      ))}
-      </List>
-    </Paper>
-   );
-   return (
-     <div className="App">
-      <Container maxWidth="md">
-        <AddTodo add={this.add}/>
-        <div className="TodoList">{todoItems}</div>
-      </Container>
-    </div>
-    );
-
-  }
+class App extends React.Component { render(){ return (
+ <div className="App">
+ <Hello />
+ </div>
+ ); }
 }
-
 export default App;
