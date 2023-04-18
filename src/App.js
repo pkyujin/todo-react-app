5f74f3c64312261b0cf64dcaf9141be351bcb845
Park Yu Jin <luxoris02@sookmyung.ac.kr>
2023-04-18 12:32:46 +0900

    깃실습8장.App수정 최종-1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import Todo from './Todo';
+import {Paper, List}from "@material-ui/core";
 import './App.css';
-class App extends React.Component { 
-  constructor(props){ 
-    super(props); 
-    this.state ={ 
-      items :[ 
-        {id:0, title:"Todo 1 ", done:false}, 
-        {id:1, title:"Todo 2 ", done:false}, 
-      ], 
-    }; 
-  } 
-  render(){  
-    var todoItems =this.state.items.map((item,idx)=>(
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      items :[
+        {id:0, title:"Todo 1", done:true},
+        {id:1, title:"Todo 2", done:false},
+      ],
+    };
+  }
+  render(){
+   var todoItems =this.state.items.length >0 &&(
+    <Paper style={{margin:16}}>
+      <List>
+      {this.state.items.map((item,idx)=>(
       <Todo item={item} key={item.id}/>
-    ));
-   return <div className="App">{todoItems}</div>; 
+      ))}
+      </List>
+    </Paper>
+   );
+   return <div className="App">{todoItems}</div>;
   }
 }
+
 export default App;
