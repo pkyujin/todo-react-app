4d63ebe3ee27d8f9a63000ce91bdb2324834574b
Park Yu Jin <luxoris02@sookmyung.ac.kr>
2023-04-18 12:28:28 +0900

    깃실습8장.App수정 최종

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import Todo from './Todo';
 import './App.css';
-class App extends React.Component { constructor(props){ 
-   super(props);
-   this.state ={ 
-     item :{id:0, title:"Hello World 1 ", done:true}, 
-    };
+class App extends React.Component { 
+  constructor(props){ 
+    super(props); 
+    this.state ={ 
+      items :[ 
+        {id:0, title:"Todo 1 ", done:false}, 
+        {id:1, title:"Todo 2 ", done:false}, 
+      ], 
+    }; 
   } 
- render(){ return (
-    <div className="App">
-      <Todo item={this.state.item}/> 
-    </div>
-   ); 
+  render(){  
+    var todoItems =this.state.items.map((item,idx)=>(
+      <Todo item={item} key={item.id}/>
+    ));
+   return <div className="App">{todoItems}</div>; 
   }
 }
 export default App;
